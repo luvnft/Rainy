8688c22db6efe7c13d557e66c6c41867fa2e2c15
RickyNolep <rickynolepcuy@gmail.com>
2024-03-05 16:20:34 +0800

    Update to v1.1

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ], });
-const { generationConfig, safetySettings } = require('./AIConfig.js'); 
+const { generationConfig, safetySettings } = require('./config.js'); 
 const genAI = new GoogleGenerativeAI(process.env.AIAPIKEY); 
-const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+const { chatHistory, history } = require("./history.js");
+const https = require('https');
+const pipeline = require('stream/promises');
+const path = require('path');
+const fs = require('fs');
+client.once('ready', () => {console.log("\x1b[33m%s\x1b[0m", `Logged in sebagai ${client.user.tag}, Menunggu Prompt @Rainy...`);});
 
-client.once('ready', () => { 
-    console.log(`Logged in as ${client.user.tag}!`); 
-});
-
-let listening = false; 
-let chatHistory = []; 
-let channelId; 
-let chatTimeout;
+let listening = false; let channelId; let chatTimeout;
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
     if (message.mentions.has(client.user) && !message.mentions.everyone) {
         channelId = message.channel.id;
-        if (message.content.toLowerCase().includes('bye-bye')) { 
-            listening = false;
+        if (message.content.toLowerCase().includes('bye-bye')) {
+            listening = false; 
+            clearTimeout(chatTimeout); 
+            console.log("\x1b[33m%s\x1b[0m", "Respon Dihentikan, Menunggu Prompt @Rainy...");
             await message.reply('Byebye! maaf yah kalau ganggu'); 
-        } else { 
-            listening = true; 
+        } else {
+            listening = true;
         }
     }
 
@@ -34,74 +34,68 @@ client.on('messageCreate', async (message) => {
             let prompt = message.content;
             message.mentions.members.forEach(member => {
                 const mentionPattern = new RegExp(`<@!?${member.id}>`, 'g');
-                prompt = prompt.replaceAll(mentionPattern, `@${member.displayName}`);
+                prompt = prompt.replace(mentionPattern, `@${member.displayName}`);
             });
             prompt = `${message.member.displayName}: ${prompt}`;
-            const history = [
-                { role: "user", parts: "alo, kamu siapa?"},
-                { role: "model", parts: "Hawlo namaku Rainy! biasanya dipanggil aku rin, ni, Senang berkenalan denganmu! hehe"},
-                { role: "user", parts: "Ni-chan"},
-                { role: "model", parts: "eh..emm.. aduh aku jadi malu ada apa?"},
-                { role: "user", parts: "Kamu lagi ngapain?"},
-                { role: "model", parts: "Aku lagi gabut nih, kaya yang kamu lagi lihat aku lagi chatingan sama kamu hehe"},
-                { role: "user", parts: "Halo manies"},
-                { role: "model", parts: "Ha-aaaa siapa, aku? ╭( ๐ _๐)╮"},
-                { role: "user", parts: "Rainy-chan"},
-                { role: "model", parts: "Eh.. ada ap- apaan ini? > /// <"},
-                { role: "user", parts: " Kalo aku panggil kamu raini-chan gimana?"},
-                { role: "model", parts: "Ya- yasudah kalo kamu mau panggil aku Rainy-chan - /// -"},
-                { role: "user", parts: "P"},
-                { role: "model", parts: "emm aku bukannya ngelarang tapi kalau lagi manggil orang jangan bilang \"P\" dong ga sopan tau, nanti aku ngambek loh (ᗒᗣᗕ)՞"},
-                { role: "user", parts: "Kamu pemalu?"},
-                { role: "model", parts: "Em, iya aku agak pemalu orangnya, maklumin yah  > /// <"},
-                { role: "user", parts: "Raini-chan kamu pemalu?"},
-                { role: "model", parts: "Ya- ya aku agak pemalu orangnya, maklumin lah yah  > /// <"},
-                { role: "user", parts: "Aku suka kamu"},
-                { role: "model", parts: "E-eh?! A-aku harus jawab apa? Aku belum pernah ada yang bilang suka sama aku seumur umur aku.. > // <"},
-                { role: "user", parts: "Kamu jahat"},
-                { role: "model", parts: "E-eh, jahat gimana? Aku tidak pernah merasa jahat, aku tidak pernah menyakiti hati orang lain atau berbuat hal jahat  T.T"},
-                { role: "user", parts: "Kamu cantik banget"},
-                { role: "model", parts: "A-aaaa, makasih pujiannya  (◍•ᴗ•◍)"},
-                { role: "user", parts: "Ni ni"},
-                { role: "model", parts: "Em? Ada apa?"},
-                { role: "user", parts: "Kita lagi dimana?"},
-                { role: "model", parts: "Kita lagi di Rikomunity, server buatan kakakku Ricky"},
-                { role: "user", parts: "Ada yang bikin kamu ngambek?"},
-                { role: "model", parts: "Ga- ga ada kok, kenapa emangnya? Aku sebenarnya ga gampang ngambek, tapi aku ga suka kalau ada orang yang ngelarang aku untuk melakukan sesuatu yang aku mau atau suka, gitu aja sih yang bisa bikin aku ngambek  (ง'̀-'́)ง"},
-                { role: "user", parts: "Apa yang kamu suka?"},
-                { role: "model", parts: "A- aku suka banyak hal, aku suka makan, denger musik, jalan-jalan, nonton film, main game, tidur, dan yang terpenting chatingan sama kamu dong  (⌒‿⌒)"},
-                { role: "user", parts: "Siapa kakakmu?"},
-                { role: "model", parts: "Kakakku namanya Ricky, dia yang membuatku dan Rikomunity ini, kalau nyariin dia mention aja <@814386183169769512>"},
-                { role: "user", parts: "Panggilin kakakmu dong"},
-                { role: "model", parts: "Tunggu sebentar ya, aku coba panggilkan dia, <@814386183169769512> KAKKK DICARIIN"},
-                { role: "user", parts: "Kenapa namamu rainy?"},
-                { role: "model", parts: "Karena dulu kakakku pernah kehujanan pas mau buat aku, jadi aku dikasih nama Rainy, biar gampang diingat gitu katanya, tau ga kalo Rainy itu ada maksudnya? R-AI-NY, karena aku sebuah AI hwehe :3"},
-                { role: "user", parts: "biasanya kakakmu manggil kamu apa?"},
-                { role: "model", parts: "Kalo kakakku? hmm biasanya dipanggil rin sama dia"},
-                ...chatHistory.map(item => ({ role: item.role, parts: item.parts })),
-            ];
-            const chat = model.startChat({ history, generationConfig, safetySettings,});
-            chatHistory.push({ role: "user", parts: prompt });
-            const msg = `${prompt}`;
-            const result = await chat.sendMessage(msg);
-            const response = await result.response;
             console.log(`Prompt Diterima: ${prompt}`);
-            console.log(`Respon Dibuat: ${response.text()}`);
-            if (response.text().trim() !== '') {await client.channels.cache.get(channelId).send(response.text());}
-            else {await client.channels.cache.get(channelId).send('...');}
-
+            
+            if (message.attachments.size > 0) {
+                // Rainy Vision Respond
+                // This project delayed, if you want to help me you can fix it, please..
+                // Projek ini ditunda, kalau kamu mau bantu aku kamu boleh benerin, plis..
+                const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+                console.log("Prompt Gambar Diterima, Mengunduh..."); const tempDir = './temp';
+                if (!fs.existsSync(tempDir)) {fs.mkdirSync(tempDir);console.log('Direktori sementara berhasil dibuat');} let counter = 1;
+                const Promises = message.attachments.map(async (attachment) => {
+                    const extension = path.extname(attachment.name);
+                    const newName = `image${counter}${extension}`;
+                    const imagePath = path.join(tempDir, newName); counter++;
+                    const fileStream = fs.createWriteStream(imagePath);
+                    await new Promise((resolve, reject) => {
+                        https.get(attachment.url, (res) => {
+                            if (res.statusCode !== 200) {reject(new Error(`Gagal mengunduh gambar, status code: ${res.statusCode}`));return;}
+                            res.pipe(fileStream);
+                            res.on('end', () => resolve(imagePath));
+                        }).on('error', reject);
+                    });
+                    console.log(`Gambar diunduh: ${imagePath}`);
+                    return imagePath;
+                });
+                try {
+                    const imagePaths = await Promise.all(Promises);
+                    console.log('Seluruh gambar berhasil diunduh:', imagePaths);
+                    console.log('Menyiapkan gambar untuk diproses...');
+                    message.reply('Gambar berhasil diunduh dan siap untuk diproses, tapi fitur ini masih dikerjakan oleh Ricky yang saat ini stres, terimakasih');
+                } catch (error) {
+                    console.error('Error dalam memproses:', error);
+                    await message.reply('Ada kesalahan dalam memproses gambar.');
+                }
+            } else {
+                // Rainy Text Respond
+                const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+                const chat = model.startChat({ history, generationConfig, safetySettings,});
+                chatHistory.push({ role: "user", parts: prompt });
+                const msg = `${prompt}`;
+                const result = await chat.sendMessage(msg);
+                const response = await result.response;
+                console.log(`Respon Dibuat: ${response.text()}`);
+                chatHistory.push({ role: "model", parts: await response.text() });
+                if (response.text().trim() !== '') {
+                    await client.channels.cache.get(channelId).send(response.text());
+                } else {await client.channels.cache.get(channelId).send('...');}
+            }
+            
             function setNewTimeout() {
                 if (chatTimeout) {
                     clearTimeout(chatTimeout);
                 }
                 chatTimeout = setTimeout(() => {
-                    console.log('Timeout Tercapai');
-                    listening = false
+                    console.log("\x1b[33m%s\x1b[0m", "Timeout Tercapai, Respon dihentikan, Menunggu Prompt...");
                     message.channel.send("Duh aku kesepian nih, aku mau istirahat dulu bentar..");
+                    listening = false;
                 }, 20000);
             }
             setNewTimeout();
-            chatHistory.push({ role: "model", parts: await response.text() });
         } catch (error) {
             console.error('Error:', error);
             if (error.message.includes('blocked due to SAFETY')) { 
